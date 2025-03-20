f996d01d942c5c51476e1d25b190f11473cbc711
CezinhaDev <cesarhenriquee04@gmail.com>
2025-03-20 20:40:12 -0300

    feat: teste de js

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 var a =5 //escopo global
 const b = 10 // constante
-let c = "teste" // eescopo local 
+let c = "teste" // eescopo local [
+
+
+
+
+var a = 5
+var d = 10
+
+
+function som(a,d){
+let soma = a+d
+return soma
+}
+
+console.log(soma(a,b))
+
+
+
+const sub = (a, b) => {
+    return a - b;
+}
